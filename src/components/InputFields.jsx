03d8263a25031c6c435e8645e2ba8e01d5d5fe01
EniolaOluwa <eebakare@gmail.com>
2022-11-05 11:31:20 +0100

    concluded on functionality, ready for styling

--- src/components/InputFields.jsx
+++ src/components/InputFields.jsx
@@ -7,23 +7,38 @@ const InputFields = () => {
 	const [lastName, setLastName] = useState('');
 	const [email, setEmail] = useState('');
 	const [message, setMessage] = useState('');
+	const [checkBox, setCheckBox] = useState(false)
 	const [error, setError] = useState(false);
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		if (firstName.length === 0 ||
+			lastName.length === 0 ||
+			message.length === 0 ||
+			checkBox === false) {
+			setError(true)
+		}
+	}
 
   return (
-		<form>
+		<form onSubmit={handleSubmit} >
 			<Stack className="input_fields" direction='column'spacing={2} >
 					<Stack direction='row' spacing={2}>
 						<Stack>
 							<label>First Name </label>
 							<input value={firstName} onChange={e => setFirstName(e.target.value )} placeholder='Enter your first name' />
-							<span>first name is required</span>
+							{error && firstName.length <= 0
+								? <span>First name is required</span>
+								: ''}
 						</Stack>
 						
 						<Stack>
 							<label>Last Name</label>
 							<input value={lastName} onChange={e => setLastName (e.target.value)} placeholder='Enter your last name' />
-							<span>last name is required</span>
+							{error && lastName.length <= 0
+								? <span>Last name is required</span>
+								: ''
+							}
 						</Stack>
 					</Stack>
 					<Stack>
@@ -32,11 +47,18 @@ const InputFields = () => {
 					</Stack>
 					<Stack>
 						<label>Message</label>
-						<input value={message} onChange={e => setMessage (e.target.value)} placeholder="Send me a message and I'll reply you as soon as possible..." type='email' />
-						<span>Please enter a message</span>
+						<input value={message} onChange={e => setMessage (e.target.value)} placeholder="Send me a message and I'll reply you as soon as possible..." type='text' />
+						{error && message.length <= 0
+							? <span>Please enter a message</span>
+							: ''
+						}
 					</Stack>
 					<Stack direction='row'>
-						<input type='checkbox'/> <span>You agree to providing your data to Eniola who may contact you.</span>
+						<input value={checkBox} onChange={e => setCheckBox(!checkBox)} type='checkbox'/> <p className='check_para' >You agree to providing your data to Eniola who may contact you.</p><br></br>
+						{error && checkBox === false
+							? <span>* this is required</span>
+							: ''
+						}
 					</Stack>
 					<Stack>
 						<button>
